32797ffb63b3558239e2f6fe40056183c54c6220
Bhargav Raju <bhargav.raju@kodebloom.com>
2026-05-05 20:58:52 +0530

    latest

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import register_routes
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,7 +11,9 @@ db.init_app(app)
 
 register_routes(app)
 
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
